48c9ff0c285cfcae4b7abc1b7d9084dc01323619
jfcarocota <jfcarocota@gmail.com>
2019-01-19 23:41:58 -0700

    commit1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 0f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * 
+            Input.GetAxis("Horizontal") * 
+            moveSpeed * Time.deltaTime);
+    }
+}
