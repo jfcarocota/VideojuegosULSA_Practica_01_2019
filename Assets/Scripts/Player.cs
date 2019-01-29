b9bf104baede648b445351001e384d7586350963
jfcarocota <jfcarocota@gmail.com>
2019-01-29 13:35:28 -0700

    commir 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,26 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed = 0f;
 
+    Animator anim;
+    SpriteRenderer spr;
+
+    float moveX;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
+        moveX = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * 
-            Input.GetAxis("Horizontal") * 
+            moveX * 
             moveSpeed * Time.deltaTime);
+
+        anim.SetFloat("MoveX", Mathf.Abs(moveX));
+
+        spr.flipX = moveX < 0f ? true : moveX > 0f ? false : spr.flipX;
     }
 }
